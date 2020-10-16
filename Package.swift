351bd395c29fc2750d2c8b95e9cb0c2d15416e0d
Dom Chapman <viralplatipuss@gmail.com>
2020-10-16 20:21:07 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .library(name: "WebsocketClientIO", targets: ["WebsocketClientIO"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/viralplatipuss/SimpleFunctional.git", .exact("0.0.10")),
+        .package(url: "https://github.com/viralplatipuss/SimpleFunctional.git", .exact("0.0.11")),
         .package(url: "https://github.com/vapor/websocket-kit.git", .exact("1.1.2")),
     ],
     targets: [
